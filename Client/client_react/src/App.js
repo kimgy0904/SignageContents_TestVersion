4b710ac681c1d31c32d116b969205419e567ca2e
DESKTOP-SOBT1B1\soda1 <PPOS2EUN@gmail.com>
2022-09-19 23:11:51 +0900

    add slick slide

--- Client/client_react/src/App.js
+++ Client/client_react/src/App.js
@@ -1,4 +1,7 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, Component} from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function App() {
   const addr = "ws://localhost:5000"
@@ -31,7 +34,7 @@ function App() {
       ws.current.onmessage = (evt) => {
         const data = JSON.parse(evt.data);
         console.log(data);
-        setOutputs((prevItems) => [data[2]]);
+        setOutputs((prevItems) => [data[1]]);
         //setOutputs((prevItems) => [data['message']]); //single message
         //setItems((prevItems) => [data]);
       };
@@ -69,4 +72,40 @@ function App() {
   );
 }
 
-export default App;
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+        <div>
+          <App/>
+          <h2> Single Item</h2>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+        </div>
+    );
+  }
+}
--- Client/client_react/src/index.js
+++ Client/client_react/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import {SimpleSlider} from './App.js';
 import reportWebVitals from './reportWebVitals';
+import SimpleSlider from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <SimpleSlider />
   </React.StrictMode>
 );
 
