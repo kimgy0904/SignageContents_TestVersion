b4e2a4a67c28e4fc9cda13dcbd7148e95db4aa0e
Park(CMS_Shelter) <junhq1234@cu.ac.kr>
2023-02-22 13:33:39 +0000

    Fix websocket connection error

--- Server/server.py
+++ Server/server.py
@@ -9,17 +9,23 @@ from advertiser import Advertiser
 
 async def accept(websocket, path):
     print('accepted', websocket.origin, websocket.id)
-    while True:
-        data = await websocket.recv()  # 클라이언트로부터 메시지를 대기한다.
-        recvdata = json.loads(data)
-        recvMsg = int(recvdata['message'])
 
-        #if you receive '0' data from client once, add client socket into Advertiser client list
-        if recvMsg == 0: #advertise mode ready to client
-            await adv.addClient(websocket)
-            await adv.printClients()
-        else:
-            print("Wrong Messages",data)
+    try:
+        while True:
+            print("before")
+            data = await websocket.recv()  # 클라이언트로부터 메시지를 대기한다.
+            recvdata = json.loads(data)
+            recvMsg = int(recvdata['message'])
+            print("after")
+
+            #if you receive '0' data from client once, add client socket into Advertiser client list
+            if recvMsg == 0: #advertise mode ready to client
+                await adv.addClient(websocket)
+                await adv.printClients()
+            else:
+                print("Wrong Messages",data)
+    except Exception as e:
+        print(e)
 
 adv = Advertiser()
 
